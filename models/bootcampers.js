579e5541cb31bec4fd938109802fdd7422779e6d
luke123adams <lxa207@gmail.com>
2022-11-22 16:06:22 +0000

    added image url to get query

--- models/bootcampers.js
+++ models/bootcampers.js
@@ -3,7 +3,7 @@ const { query } = require("../db/index.js");
 async function getBootcampers() {
   
     const strengthsArray = await query("SELECT first_name, last_name, topic_name, strength_weakness FROM strengths_weaknesses s JOIN bootcampers b ON b.bootcamper_id = s.bootcamper_id JOIN topics t ON t.topic_id = s.topic_id;")
-    const bootcamperArray = await query("SELECT first_name, last_name, password FROM bootcampers")
+    const bootcamperArray = await query("SELECT first_name, last_name, password, image_url FROM bootcampers")
     return [bootcamperArray.rows, strengthsArray.rows];
   }
 
