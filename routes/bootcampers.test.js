35da753336e87dc672c6decf27dda7b1e4f6de94
Chet Jogia <chetjogia@gmail.com>
2022-11-24 09:55:14 +0000

    connection with server for supertest is now working

--- routes/bootcampers.test.js
+++ routes/bootcampers.test.js
@@ -4,10 +4,14 @@ const { expect, test } = require ("@jest/globals")
 const { pool } = require('../db/index')
 // const  supertest from "supertest";
 
-// describe ('get handler test suite', () => {
-   test('responds to get request with 200 status code', async () => {
-   const response = await request(app).get("/bootcampers/")
-   // expect(response.status).toBe(200);
-    expect(response.body).toStrictEqual({success: true, payload: expect.any(Array)});
+describe ('get handler test suite', () => {
+   afterAll(()=>{pool.end()})
+   
+   test('responds to get request with 200 status code', async function(){
+   const response = await request(app).get("/api/bootcampers/")
+   expect(response.status).toBe(200);
+    //expect(response.body).toStrictEqual({success: true, payload: expect.any(Array)});
+   
    })
-// })
+ })
+
